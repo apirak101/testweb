cdd59d9dabfbc96020de4d79145ccf2fef696a81
apirak101 <6306021420132@fitm.kmutnb.ac.th>
2021-03-09 15:55:22 +0700

    update

--- db.php
+++ db.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DCDOG</title>
-</head>
-<body>
-    <h1>asdasdasd</h1>
-    <h1>asdasdasd</h1>
-</body>
-</html>
+
+<?php 
+
+    $con = mysqli_connect("localhost","test2_admin","QgmfYeHAV2HSpUyS","test2") or die("Error db");
+
+    if($con){
+        echo "เชื่อมต่อสำเร็จ";
+    }
+?>
