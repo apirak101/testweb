a81ccee361b6dd2cbc512556b6ea9b0bb7a6f2bf
Your Name <6306021412113@fitm.kmutnb.ac.th>
2021-03-12 15:12:31 +0700

    update

---
+++ restful.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body onload="loadDoc()">
+<script>
+    function loadDoc(){
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){ // การแจ้งสถานะว่าเว็บเราถึงขั้นตอนไหนแล้ว
+            console.log(this.readyState);
+            if(this.readyState==4 && this.status == 200){
+            console.log(this.responseText);
+            let data = JSON.parse(this.responseText);
+            console.log(data);
+            }
+        }
+        xhttp.open("GET","reset.php",true);
+        xhttp.send();
+    }
+    </script>
+</body>
+</html>
