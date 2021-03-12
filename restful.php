ad43bc93769c62e6e353e22dc0031e5eb4744d0a
Your Name <6306021412113@fitm.kmutnb.ac.th>
2021-03-12 16:05:36 +0700

    update

--- restful.php
+++ restful.php
@@ -6,16 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body onload="loadDoc()">
-<script>
+<body onload = "loadDoc()">
+    <div id="out"></div>
+    <script>
     function loadDoc(){
         let xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function(){ // การแจ้งสถานะว่าเว็บเราถึงขั้นตอนไหนแล้ว
             console.log(this.readyState);
             if(this.readyState==4 && this.status == 200){
             console.log(this.responseText);
-            let data = JSON.parse(this.responseText);
-            console.log(data);
+            my = JSON.parse(this.responseText);
+            m = document.getElementById("out");
+            text="<table border='1'>";
+            for(i=0;i<my.length;i++){
+                text+"<tr>";
+                for(key in my[i]){
+                text+="<td>"+my[i][key]+"</td>";
+                }
+                text+="</tr>";
+            }
+            text+="</table>";
+            m.innerHTML += text;
+            //console.log(data);
             }
         }
         xhttp.open("GET","rest.php",true);
